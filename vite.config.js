6dd2c74a8c815896b111fa956da4dfb1b33ed4c4
Sid <130921999+pegasus102@users.noreply.github.com>
2025-10-20 01:58:31 +0530

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANT: Change 'my-portfolio' to your exact GitHub repository name
+  // This is the crucial line for a username.github.io repository
   base: '/', 
   build: {
     target: 'esnext'
